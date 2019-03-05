de4fd5130969c9fa12eeb7254a5e2f890227e42c
nguyinh <florent.dupontnguyen@gmail.com>
2019-03-05 11:53:12 +0100

    Added Mongoose

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
+require("dotenv").config()
+
+
+const mongoURL = process.env.MONGODB_URI || "mongodb://localhost/klih"
+mongoose.connect(mongoURL, {
+  useNewUrlParser: true
+})
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, '[MongoDB] Error during connection'));
+db.once('open', function() {
+  console.log('\x1b[36m%s\x1b[0m', `[MongoDB] Connected to {port}`);
+});
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
@@ -20,4 +34,4 @@ app.get('*', (req, res) => {
 const port = process.env.PORT || 8116;
 app.listen(port);
 
-console.log('Server listening on ${port}');
+console.log('\x1b[36m%s\x1b[0m', `[Express] Server listening on ${port}`);
