e2dd2f3b2db4b556e2921f4be717eae4e55dab13
nguyinh <florent.dupontnguyen@gmail.com>
2019-03-05 10:56:53 +0100

    Add React

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,30 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    working: false
+  }
+
+  componentDidMount() {
+    this.fetchAPI();
+  }
+
+  fetchAPI = () => {
+    fetch('/api/test').then(res => {
+      this.setState({working: true})
+    })
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return (<div className="App">
+      {
+        this.state.working
+          ? <h1>Works !</h1>
+          : <h1>Don't work</h1>
+      }
+      </div>);
   }
 }
 
