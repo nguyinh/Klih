bed19b96843517ad0182e0511c1f261e51d00062
nguyinh <florent.dupontnguyen@gmail.com>
2019-05-30 11:49:05 +0200

    Debug prod HTTPS

--- middlewares/forceHttps.js
+++ middlewares/forceHttps.js
@@ -1,5 +1,6 @@
 module.exports = (req, res, next) => {
-  if (req.secure) {
+  console.log('req secure : ' + req.secure);
+  if (req.secure || req.get('X-Forwarded-Proto') !== 'https') {
     return next();
   } else {
     return res.redirect(`https://${req.get('host')}${req.originalUrl || req.url}`);
